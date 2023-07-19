b12cacb1cdf66911fe7deea387cc037863af7b2d
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-18 23:44:11 -0500

    fixed themeTypes

--- client/src/themeTypes.ts
+++ client/src/themeTypes.ts
@@ -1,7 +1,4 @@
-// import {
-//   Palette,
-//   PaletteColor,
-// } from "@mui/material/styles/createPalette";
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
