54572ad7b70999220b17d659c629b15d19a17fef
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-18 23:57:20 -0500

    added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,27 @@
-import { useMemo } from "react";
+import { Navbar } from "@/components/Navbar";
+import { themeSettings } from "@/theme";
+import { Box, CssBaseline } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
-import { CssBaseline } from "@mui/material";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="app">hello world</div>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Box width={"100%"} height={"100%"} p={"1rem 2rem 4rem 2rem"}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard</div>} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
