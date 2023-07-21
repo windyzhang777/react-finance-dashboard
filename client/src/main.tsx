09cf467eab4481aa89de3f870df0df053c62f0f6
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-20 19:06:54 -0500

    added redux store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
+import { api } from "@/state/api";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
